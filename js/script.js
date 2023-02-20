b4078f054101d6620637cf929ba874916a5404cb
GuidtJ <jeromeguidt@hotmail.fr>
2023-02-20 16:46:24 +0100

    push

--- js/script.js
+++ js/script.js
@@ -1,64 +1,23 @@
-// const grandEst = document.getElementById("region-A");
-// const infoGrandEst = document.querySelector(".infoGrandEst");
-// const aquitaine = document.getElementById("region-B");
-// const infoAquitaine = document.querySelector(".infoAquitaine");
-// const auvergne = document.getElementById("region-C");
-// const infoAuvergne = document.querySelector(".infoAuvergne-Rhône-Alpes");
-
-// grandEst.addEventListener("click", () => {
-//    infoAquitaine.innerHTML = '';
-//    infoAuvergne.innerHTML = '';
-//    infoGrandEst.innerHTML = "Région : Grand-Est";
-
-// });
-
-// aquitaine.addEventListener("click", () => {
-//     infoAuvergne.innerHTML = '';
-//     infoGrandEst.innerHTML = '';  
-//     infoAquitaine.innerHTML = "Région : Aquitaine";
-// });
-
-// auvergne.addEventListener("click", () =>{
-//     infoAquitaine.innerHTML = '';
-//     infoGrandEst.innerHTML = '';
-//     infoAuvergne.innerHTML = "Région : Auvergne";
-
-// });
-// const regionName = {
-//   grandEst: {
-//     name: "Grand-Est",
-    
-//   },
-//   aquitaine: {
-//     name: "Aquitaine",
-    
-//   },
-//   auvergne: {
-//     name: "Auvergne",
-    
-//   }
-// };
-
-
-
 const regions = document.querySelectorAll('path');
-// const gameMap = document.getElementById('gameMap');
-// const infoRegion = document.querySelector('.infoRegion');
+const gameMap = document.getElementById('gameMap');
+const infoRegion = document.querySelector('.infoRegion');
 let regionsample = []
-// const randomIndex = Math.floor(Math.random() * regions.length);
-// const randomRegion = regions [randomIndex];
-
-// console.log(randomIndex);
-// console.log(randomRegion);
+const randomIndex = Math.floor(Math.random() * regions.length);
+const randomRegion = regions [randomIndex];
+console.log(randomIndex);
+console.log(randomRegion);
 
 
      regions.forEach(region => {
         regionsample.push(region.getAttribute('title'))
-        //  region.addEventListener("click",() => {
+        
+          region.addEventListener("click",() => {
             
-        //      let title = region.getAttribute('title');
-        //      infoRegion.innerHTML = title;
+              let title = region.getAttribute('title');
+              infoRegion.innerHTML = title;
             
-        //  });
+          });
      });
+
+
 console.table(regionsample);
