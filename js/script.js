aaaec80c8c9fb951cc751e4fe2cc4e894cc31ac1
GuidtJ <jeromeguidt@hotmail.fr>
2023-02-21 13:48:45 +0100

    quiz

--- js/script.js
+++ js/script.js
@@ -1,23 +1,72 @@
+
 const regions = document.querySelectorAll('path');
-const gameMap = document.getElementById('gameMap');
+const results = document.querySelector('.results');
 const infoRegion = document.querySelector('.infoRegion');
+const restartBtn = document.querySelector('.restartBtn');
 let regionsample = []
-const randomIndex = Math.floor(Math.random() * regions.length);
-const randomRegion = regions [randomIndex];
-console.log(randomIndex);
-console.log(randomRegion);
+let randomIndex, randomRegion;
 
+     function getRandomRegion(){
+          randomIndex = Math.floor(Math.random() * regions.length);
+          randomRegion = regions[randomIndex].getAttribute('title');
+          infoRegion.textContent = 'Cliquez sur la région : ' + randomRegion + '';
+     }
 
+     function RegionClick(event){
+
+          const selectedRegion = event.target.getAttribute('title');
+     
+          if (selectedRegion === randomRegion){
+               results.textContent = "Bravo vous avez trouver : " + randomRegion + " 😀 ";
+               getRandomRegion();
+          }
+          else {
+               results.textContent = "Dommage ce n'est pas la régions : " + selectedRegion + " 😟 ";
+          }
+     
+     }
+
+     // function RegionClick(event){
+
+     //      const selectedRegion = event.target.getAttribute('title');
+
+     //      if (selectedRegion === randomRegion){
+     //           alert('Bravo ' + randomRegion +' 😀 ');
+     //           getRandomRegion();
+     //      }
+     //      else {
+     //           alert('Dommage ' + selectedRegion + ' 😟' );
+     //      }
+
+     // }
+
+     
      regions.forEach(region => {
-        regionsample.push(region.getAttribute('title'))
+          regionsample.push(region.getAttribute('title'));
+          region.addEventListener('click', RegionClick);
+      });
+      restartBtn.addEventListener('click', getRandomRegion);
+
+getRandomRegion();
+
+
+
+
+     // regions.forEach(region => {
+     //    regionsample.push(region.getAttribute('title'))
         
-          region.addEventListener("click",() => {
+     //      region.addEventListener("click",() => {
             
-              let title = region.getAttribute('title');
-              infoRegion.innerHTML = title;
+     //          let title = region.getAttribute('title');
+     //          infoRegion.innerHTML = title;
             
-          });
-     });
+     //      });
 
 
+     // });
+     
+     
+console.log(randomIndex);
+ console.log(randomRegion);
 console.table(regionsample);
+
